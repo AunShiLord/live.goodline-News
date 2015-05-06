fccd8a0ea4842c15aec8bc9f77de0522604d5a33
Admin <admin@Mac-Admin.local>
2015-05-06 08:46:48 +0300

    Added NewsListTableView class

--- live.goodline News/AppDelegate.m
+++ live.goodline News/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import "newsList.h";
+#import "NewsListTableView.h";
 
 @interface AppDelegate ()
 
---
+++ live.goodline News/NewsListTableView.h
@@ -0,0 +1,13 @@
+//
+//  NewsListTableView.h
+//  live.goodline News
+//
+//  Created by Admin on 06.05.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsListTableView : UITableViewController
+
+@end

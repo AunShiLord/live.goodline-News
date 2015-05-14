b5eb76210e3511c935faf661922d4b7baaf8eb34
Admin <admin@Mac-Admin.local>
2015-05-14 13:54:58 +0300

    Some works on widget

---
+++ Last news widget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  Last news widget
+//
+//  Created by Admin on 14.05.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
